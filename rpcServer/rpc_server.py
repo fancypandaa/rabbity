ea2a261d1ce22a6018df3dcf8ca442db5f484244
liosub <mlio64565@gmail.com>
2023-08-03 23:58:30 +0100

    first stage in rpc+rmq server

---
+++ rpcServer/rpc_server.py
@@ -0,0 +1,26 @@
+import pika
+
+class Rpcserver:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost')
+        )
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='rpc_queue')
+
+    def on_request(self,ch,method,props,body):
+        n = int(body)
+        print(n)
+        response=n
+        ch.basic_publish(exchange='',
+        routing_key=props.reply_to,
+        properties = pika.BasicProperties(correlation_id=props.correlation_id),
+        body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    
+    def prompt_init(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue='rpc_queue',on_message_callback=self.on_request)
+        print(" [x] Awaiting RPC requests")
+        self.channel.start_consuming()
+
