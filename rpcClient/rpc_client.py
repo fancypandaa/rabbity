be15f346b7b1b1c3f1921377274fee208379c12a
liosub <mlio64565@gmail.com>
2023-10-15 22:30:51 +0100

    clean rpc_client no spaghetti any more

--- rpcClient/rpc_client.py
+++ rpcClient/rpc_client.py
@@ -3,6 +3,7 @@ import uuid
 from ast import literal_eval
 from networkInfo import NetworkInfo
 import ipaddress  
+from jobs.serpent_creator import SerpentCreator
 
 class RpcClient(NetworkInfo):
     def __init__(self,default_queue='galaxy'):
@@ -30,79 +31,25 @@ class RpcClient(NetworkInfo):
             print("Your IP Adrress is Valid!!")
         except ValueError:
             print("You Enter wrong IP XXX")
-
-    def check_plain_text_length(self,plain_text):
-        x = plain_text.split()
-        number_of_words= len(x)
-        return number_of_words
-
-    def check_cipher_text(self,cipher_text):
-        p = set(cipher_text)
-        s= {'0','1'}
-        if s == p or p == {'0'} or p == {'1'}:
-            return True
-        else:
-            return False
-    
-    def _serpent_encrypt_builder(self):
-            print("Enter your message between 5 to 100 words!!")
-            plain_text = input()
-            number_of_words= self.check_plain_text_length(plain_text)
-            if number_of_words >= 5 and number_of_words <= 100:
-                self.users["plain_text"]= plain_text
-                print(f'{number_of_words}/100 accepted length...')
-                user_key_option= input("choose auto generate key: y/N: ")
-                if user_key_option.lower() == "y":
-                    self.users["auto_generate_key"]=True
-                elif user_key_option.lower() == "n":
-                    user_key = input("Enter Your Key between 128bit and 256bit")
-                    self.users["auto_generate_key"]=False
-                    self.users["user_key"]= user_key
-                else:
-                    print("FUCK OFF!!!")
-            else:
-                print(f'{number_of_words}/100 exceed limit FUCK YOU...')
-                exit()
-    
-    def _serpent_decrypt_builder(self):
-            print("Enter your Cipher")
-            cipher_text = input()
-            if self.check_cipher_text(cipher_text):
-                self.users["cipher_text"]= cipher_text
-                user_key = input("Enter Your Key between 128bit and 256bit\n")
-                self.users["auto_generate_key"]=False
-                self.users["user_key"]= user_key
-            else:
-                print(f'wrong format FUCK YOU...')
-                exit()
-
+           
+            
     def user_input(self,default_queue):
         plain_text=""
         if default_queue == 'galaxy':
             print("Enter URI you want find it? ")
             s = input()
         elif default_queue == 'serpent':
-            option= input("Choose Encrption or Decrption: e/D: ")
-            if option.lower() == "e" :
-                self.users["option"] = "-"+option.lower()
-                self._serpent_encrypt_builder()
-            elif option.lower() == "d":
-                self.users["option"] = "-"+option.lower()
-                self._serpent_decrypt_builder()
-            else:
-                print("Wrong option, No comments.....")
-                exit()
-            # print(self.users)
-            
-            
-            
+            self.serpent = SerpentCreator(self.users)
+            self.users = self.serpent.user_input()
 
     def on_response(self,ch, method , props,body):
         if self.corr_id == props.correlation_id:
-            body = literal_eval(body.decode('utf-8'))
-            self.response = body
-            print(self.response[0][1].decode('utf-8'))
-
+            # body = literal_eval(body.decode('utf-8'))
+            # self.response = body
+            if self.default_queue == 'serpent':
+                self.serpent.format_serpent_output(body)
+            else:
+                exit()
 
     def call(self):
         self.response = None
