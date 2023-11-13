b24462486421ff6d2e0d93f3b7c2c0746f6e2c6e
liosub <mlio64565@gmail.com>
2023-11-13 18:49:36 +0000

    add multi processing options

--- rpcServer/args.py
+++ rpcServer/args.py
@@ -1,7 +1,33 @@
 import argparse
 from rpc_server import RpcServer
 from queues import Queues
-from celery import Celery
+# from celery import Celery
+import multiprocessing 
+
+def serpentWorker():
+    RpcServer("serpent")
+    
+def ecdsaWorker():
+    RpcServer("ecdsa")
+
+def mulitProcessing():
+    p1 = multiprocessing.Process(target=serpentWorker) 
+    p2 = multiprocessing.Process(target=ecdsaWorker) 
+    # starting processes 
+    p1.start() 
+    p2.start() 
+    # process IDs 
+    print("ID of process p1: {}".format(p1.pid)) 
+    print("ID of process p2: {}".format(p2.pid)) 
+    p1.join() 
+    p2.join() 
+        # both processes finished 
+    print("Both processes finished execution!")   
+    # check if processes are alive 
+    print("Process p1 is alive: {}".format(p1.is_alive())) 
+    print("Process p2 is alive: {}".format(p2.is_alive())) 
+    
+
 
 parser = argparse.ArgumentParser(description='RPC Rabbity Server')
 parser.add_argument('--queue','-q',type=str)
@@ -20,7 +46,7 @@ elif args.helps:
     print("under works")
 elif args.queue:
     if args.queue == '*':
-        print("nn")
+        mulitProcessing()
     elif (queues.check_queue(args.queue)):
         server = RpcServer(args.queue)
     else:
